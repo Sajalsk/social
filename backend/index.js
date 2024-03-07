35a392772457bfc3c6efd0bcf2ba9be31dbb02e4
Sajalsk <sajalsk247@gmail.com>
2024-03-07 12:16:05 +0530

    update cors

--- backend/index.js
+++ backend/index.js
@@ -7,15 +7,26 @@ ConnectMongo();
 const app = express()
 const port = 5000
 
+const corsOptions = {
+  origin: "https://social-roan-seven.vercel.app/login",
+  method:["POST","GET"],
+  credentials: true,
+};
+
+
 app.use(express.json())
 
-app.use(cors())
+app.use(cors(corsOptions));
 app.use(express.json())
 
 // Available routes
 
 app.use('/api/auth',  require('./routes/auth'))
 app.use('/api/notes', require('./routes/notes'))
+
+app.get('/', (req, res) => {
+  res.send('Hi there from server');
+});
 
 app.listen(port, () => {
   console.log(`iNotebook backend listening on port http://localhost:${port}`)
